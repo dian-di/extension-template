353655d2e4f2e137a083bea51fb96baf6094e620
yang <3257670+zhangpanrobot@users.noreply.github.com>
2026-09-16 17:46:59 +0800

    update config

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite'
-// import react from '@vitejs/plugin-react'
 import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
@@ -18,7 +17,6 @@ export default defineConfig({
   },
   vite: () => ({
     plugins: [
-      // react(),
       tailwindcss(),
     ],
   }),
